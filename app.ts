d43d6ecb16f50568fb6a98c576c67b341cb1d973
Chris ter Beke <christerbeke@binx.io>
2022-10-23 15:21:09 +0200

    Only allow a single oauth2 at a time

--- app.ts
+++ app.ts
@@ -5,7 +5,6 @@ class EnphaseApp extends OAuth2App {
 
   static OAUTH2_CLIENT = EnphaseOAuth2Client
   static OAUTH2_DEBUG = true
-  static OAUTH2_MULTI_SESSION = true
 }
 
 module.exports = EnphaseApp
