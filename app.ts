05b8296ccd799139c0b96717c2c8a52f1118d3e2
Chris ter Beke <christerbeke@binx.io>
2022-10-23 16:51:09 +0200

    Validate app in GitHub Actions

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ import { EnphaseOAuth2Client } from './lib/EnphaseOAuth2Client'
 class EnphaseApp extends OAuth2App {
 
   static OAUTH2_CLIENT = EnphaseOAuth2Client
+  static OAUTH2_MULTI_SESSION = true
 }
 
 module.exports = EnphaseApp
