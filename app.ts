9803ad91bc5fdbbe0fae3a9bfb7a1abbdffc5fdf
Chris ter Beke <christerbeke@binx.io>
2022-10-24 09:24:13 +0200

    Set up Sentry logging

--- app.ts
+++ app.ts
@@ -1,10 +1,15 @@
 const { OAuth2App } = require('homey-oauth2app')
+const { Log } = require('homey-log')
 import { EnphaseOAuth2Client } from './lib/EnphaseOAuth2Client'
 
 class EnphaseApp extends OAuth2App {
 
   static OAUTH2_CLIENT = EnphaseOAuth2Client
   static OAUTH2_MULTI_SESSION = true
+
+  async onOAuth2Init() {
+    this.homeyLog = new Log({ homey: this.homey });
+  }
 }
 
 module.exports = EnphaseApp
